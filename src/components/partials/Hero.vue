9087c20d5afde1f934f2483bfd0d1f4e5c964c4a
Corfaya <lauracorrado0696@gmail.com>
2024-07-30 17:52:41 +0200

    fix: button position

--- src/components/partials/Hero.vue
+++ src/components/partials/Hero.vue
@@ -4,12 +4,27 @@ export default {
 }
 </script>
 <template>
-    <div class="bg-hero position-relative"></div>
+    <div class="bg-hero">
+        <button class="btn main-btn text-uppercase fw-bolder text-white rounded-0 position-relative">Current Series</button>
+    </div>
 </template>
-<style lang="scss">
+<style lang="scss" scoped>
+@use "../../styles/partials/variables";
     .bg-hero {
         background-image: url("../../assets/jumbotron.jpg");
         background-size: cover;
-        height: 500px;
+        height: 400px;
+        .main-btn {
+            left: 10%;
+            top: 92%;
+            padding: 10px 20px;
+            font-size: 24px;
+            left: 10%;
+            background-color: variables.$primary_color;
+            transition: all 0.3 ease;
+            &:hover {
+                background-color: darken(variables.$primary_color, 10%);
+            }
+        }
     }
 </style>
