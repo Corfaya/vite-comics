5e0af5769336794bc2af788e9e806f720c4d34d0
Corfaya <lauracorrado0696@gmail.com>
2024-07-30 16:21:54 +0200

    [UPD] ComicsSection subcomponent's layout added

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -2,12 +2,17 @@
 import BlueSection from './partials/Blue-section.vue'
 import Hero from './partials/Hero.vue';
 import ComicsSection from './partials/ComicsSection.vue';
-
+import comics from '../data/comicsList';
 export default {
     components: {
         Hero,
         ComicsSection,
         BlueSection
+    },
+    data() {
+        return {
+            comics
+        }
     }
 }
 </script>
@@ -17,9 +22,7 @@ export default {
         <div class="bg-color">
             <div class="container">
                 <div class="row">
-                    <div class="col-12">
-                            <ComicsSection />
-                    </div>
+                    <ComicsSection v-for="thumb, index in comics" :key="`t-${index}`" :cover="thumb"/>
                 </div>
             </div>
         </div>
