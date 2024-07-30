0ee37bc47f1541864817ad6e0b428634623d8d10
Corfaya <lauracorrado0696@gmail.com>
2024-07-30 14:18:36 +0200

    [UPD] subcomponent created and imported in Main.vue

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,19 +1,33 @@
 <script>
+import BlueSection from './partials/Blue-section.vue';
+
 export default {
-    
+    components: {
+        BlueSection
+    }
 }
 </script>
 <template>
-    <main class="position-relative z-3">
-        <div class="py-5 bg-color">
+    <main>
+        <div class="bg-color">
             <div class="container">
                 <div class="row">
                     <div class="col-12">
-                            <h2 class="text-light">--> Content goes here <--</h2>
+                            <h2 class="text-light py-5">--> Content goes here <--</h2>
                     </div>
                 </div>
             </div>
         </div>
+        <div class="bg_blue">
+            <div class="container">
+                 <div class="row">
+                    <div class="col-12">
+                        <BlueSection />
+                    </div>
+                </div>
+            </div>
+        </div>
+        
     </main>
 </template>
 <style lang="scss" scoped>
@@ -21,6 +35,10 @@ export default {
     main {
         .bg-color {
             background-color: variables.$main_bg;
+        }
+
+        .bg_blue {
+            background-color: variables.$primary_color;
         }
     }
 </style>
